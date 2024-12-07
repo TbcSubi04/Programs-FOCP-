3d917a2397eefee51feffbdb8efde8e176d337d2
Subi Gurung <gsubi23@tbc.edu.np>
2024-12-07 11:24:35 +0545

    practice

--- WEEK 2 program.py
+++ WEEK 2 program.py
@@ -19,3 +19,8 @@ count=int(input("Enter the number of pupil :"))
 sweets=int(input("Enter how many sweets :"))
 distribute=sweets/count
 print(f"Each pupil will get {distribute} sweets")
+
+#2)temperature
+Fahren=float(input("Enter the temperature in Celsius :"))
+temp=(Fahren*(9/5))+32
+print(f"{Fahren}C is equivalent to {temp}F")
